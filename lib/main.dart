b19f58a6649ebefe1518a717b74a50b9b72372de
bavlysafwatt <bavlysafwatt@gmail.com>
2024-08-18 13:47:13 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+        useMaterial3: false,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
