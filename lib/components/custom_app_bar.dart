25670312d0208a5ec3595127fe71ec4fc06b8702
bavlysafwatt <bavlysafwatt@gmail.com>
2024-08-18 15:09:40 +0300

    create appbar

---
+++ lib/components/custom_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 28,
+            fontFamily: 'Ubuntu',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: Colors.white12.withOpacity(0.08),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: const Center(
+            child: Icon(
+              Icons.search,
+              size: 30,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/components/custom_app_bar.dart';
 
 class NotesPage extends StatelessWidget {
   const NotesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 8,
+          ),
+          child: Column(
+            children: [
+              CustomAppBar(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
