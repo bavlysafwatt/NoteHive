308d9c9cb8e12ec96b7bb6eb4df7eebba4f11b13
bavlysafwatt <bavlysafwatt@gmail.com>
2024-08-19 13:42:36 +0300

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/pages/notes_page.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('notesBox');
   runApp(const NotesApp());
 }
 
