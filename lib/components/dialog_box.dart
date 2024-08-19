a737e4d68f7851df5b8274fcf39966be72212591
bavlysafwatt <bavlysafwatt@gmail.com>
2024-08-19 17:33:03 +0300

    provide add note cubit to add note form

--- lib/components/dialog_box.dart
+++ lib/components/dialog_box.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/components/add_note_form.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
@@ -20,9 +23,25 @@ class DialogBox extends StatelessWidget {
           color: Colors.black12,
           borderRadius: BorderRadius.circular(10),
         ),
-        child: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: AddNoteForm(),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+            listener: (context, state) {
+              if (state is AddNoteFailure) {
+                print('Failed ${state.erroeMessage}');
+              }
+
+              if (state is AddNoteSuccess) {
+                Navigator.pop(context);
+              }
+            },
+            builder: (context, state) {
+              return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoading ? true : false,
+                child: const AddNoteForm(),
+              );
+            },
+          ),
         ),
       ),
     );
