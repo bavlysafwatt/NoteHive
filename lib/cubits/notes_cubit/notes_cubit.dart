e79a9b4d492559b14b85c032d27cb10b07dab1ae
bavlysafwatt <bavlysafwatt@gmail.com>
2024-08-20 12:17:03 +0300

    create notes cubit and states

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+}
