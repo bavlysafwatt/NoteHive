3963352c09bdbc73a2250bf78b0f6f0de91f0399
bavlysafwatt <bavlysafwatt@gmail.com>
2024-08-20 12:29:30 +0300

    create read notes method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
 
@@ -8,4 +9,13 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  readAllNotes() {
+    try {
+      var notesBox = Hive.box<NoteModel>('notesBox');
+      emit(NotesSuccess(notesList: notesBox.values.toList()));
+    } catch (e) {
+      emit(NotesFailure(erroeMessage: e.toString()));
+    }
+  }
 }
--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/components/custom_app_bar.dart';
 import 'package:notes_app/components/dialog_box.dart';
 import 'package:notes_app/components/notes_list.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 
 class NotesPage extends StatelessWidget {
   const NotesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8),
-          child: Column(
-            children: [
-              CustomAppBar(
-                title: 'Notes',
-                icon: Icons.search,
-              ),
-              NotesList(),
-            ],
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: SafeArea(
+        child: Scaffold(
+          body: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8),
+            child: Column(
+              children: [
+                CustomAppBar(
+                  title: 'Notes',
+                  icon: Icons.search,
+                ),
+                NotesList(),
+              ],
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.grey.shade900,
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) => const DialogBox(),
-            );
-          },
-          child: const Icon(Icons.add),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.grey.shade900,
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => const DialogBox(),
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
         ),
       ),
     );
