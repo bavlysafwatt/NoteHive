906a849cf47d60e0af43a8f19cb0ef0244831134
bavlysafwatt <bavlysafwatt@gmail.com>
2024-08-21 19:05:13 +0300

    change search state before adding notes

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/components/dialog_box.dart';
 import 'package:notes_app/components/notes_page_body.dart';
+import 'package:notes_app/cubits/app_bar_cubit/app_bar_cubit.dart';
 
 class NotesPage extends StatelessWidget {
   const NotesPage({super.key});
@@ -13,6 +15,7 @@ class NotesPage extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.grey.shade900,
           onPressed: () {
+            BlocProvider.of<AppBarCubit>(context).changeStateNotSearching();
             showDialog(
               context: context,
               builder: (context) => const DialogBox(),
